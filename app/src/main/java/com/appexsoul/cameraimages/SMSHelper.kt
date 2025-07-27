4b3b4c0771a0ed3600ed3df6132a02713c7c6379
Smart-cam-GmbH <64581571+Smart-cam-GmbH@users.noreply.github.com>
2025-07-27 12:37:22 +0200

    Refactor SMS command sending

--- app/src/main/java/com/appexsoul/cameraimages/SMSHelper.kt
+++ app/src/main/java/com/appexsoul/cameraimages/SMSHelper.kt
@@ -1,11 +1,10 @@
 package com.appexsoul.cameraimages
 
-import android.content.Context
 import android.telephony.SmsManager
 import android.telephony.SubscriptionManager
 
 object SMSHelper {
-    fun sendCommand(context: Context, phone: String, command: String) {
+    fun sendCommand(phone: String, command: String) {
         val subscriptionId = SubscriptionManager.getDefaultSmsSubscriptionId()
         val manager = SmsManager.getSmsManagerForSubscriptionId(subscriptionId)
         manager.sendTextMessage(phone, null, command, null, null)
